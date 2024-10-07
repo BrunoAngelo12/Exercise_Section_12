3da839122bb63a677129b54942e569503d71a9df
Bruno Angelo <bruno.trabalhos@outlook.com>
2024-10-07 20:40:52 -0300

    criado objeto date para o cliente

--- app/Exercise12.java
+++ app/Exercise12.java
@@ -1,5 +1,30 @@
+package app;
+
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
 public class Exercise12{
-    public static void main(String[] args) {
-        System.out.println("teste");
+    public static void main(String[] args) throws ParseException {
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter the client data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        String dateClientStr = sc.nextLine();
+
+        Date birthDate = (Date) fmt.parse(dateClientStr);
+
+        System.out.println(birthDate);
+
+        sc.close();
     }
 }
